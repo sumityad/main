92cd3e7acb3959a6e8552f0a2bceff0ff05dd975
Sumit Yadav <sumityadav@gmail.com>
2018-12-30 14:30:09 -0800

    Added hello world with associated CMake files. Added VS code json files to build and debug using WSL or MinGW-64

---
+++ apps/hello_world/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << endl << "Hello World" << endl;
+    for ( auto i = 0; i < 1000; i++) {
+        cout << i << " ";
+    }
+    
+}
